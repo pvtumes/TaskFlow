7ce2f66f242e0ce8547d8d48debf235271090378
Umesh Prasad <pvtumes@gmail.com>
2026-05-06 00:17:40 +0530

    Change database connection password
    Updated the database connection password for security.

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/taskflow";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Umesh@1605";
+    private static final String PASSWORD = "Your PassWord";
 
 
     private DatabaseConnection() {}
